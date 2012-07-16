8ee09b6c3b26c637957228f694f39c1a2889ae5e
Greg Wilson <greg.wilson@gmail.com>
2012-07-16 17:11:07 -0400

    Added code to address the situatin where the device sends an old cached location with a current timestamp.

--- geo.js
+++ geo.js
@@ -1,14 +1,18 @@
-/* by Greg Wilson June 2012 - http://gregsramblings.com */
-navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess, geolocationError, options)
-{
+navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess, geolocationError, geoprogress, options) {
     var lastCheckedPosition;
+    var locationEventCount = 0;
 
     var checkLocation = function (position) {
         lastCheckedPosition = position;
-        if (position.coords.accuracy <= options.desiredAccuracy) {
+        ++locationEventCount;
+        // We ignore the first event unless it's the only one received because some devices seem to send a cached
+        // location even when maxaimumAge is set to zero
+        if ((position.coords.accuracy <= options.desiredAccuracy) && (locationEventCount > 0)) {
             clearTimeout(timerID);
             navigator.geolocation.clearWatch(watchID);
             foundPosition(position);
+        } else {
+            geoprogress(position);
         }
     }
 
@@ -23,14 +27,15 @@ navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess,
         geolocationError(error);
     }
 
-    var foundPosition = function(position) {
+    var foundPosition = function (position) {
         geolocationSuccess(position);
     }
 
-    if(!options.maxWait)            options.maxWait = 10000; // Default 10 seconds
-    if(!options.desiredAccuracy)    options.desiredAccuracy = 20; // Default 20 meters
-    if(!options.timeout)            options.timeout = options.maxWait; // Default to maxWait
+    if (!options.maxWait)            options.maxWait = 10000; // Default 10 seconds
+    if (!options.desiredAccuracy)    options.desiredAccuracy = 20; // Default 20 meters
+    if (!options.timeout)            options.timeout = options.maxWait; // Default to maxWait
 
+    options.maximumAge = 0; // Force current locations only
     options.enableHighAccuracy = true; // Force high accuracy (otherwise, why are you using this function?)
 
     var watchID = navigator.geolocation.watchPosition(checkLocation, onError, options);
