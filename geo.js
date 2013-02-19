6dad66c722b2f4471571db064810452fbdbcc167
Eric Chaves <eric@craftti.com.br>
2013-02-19 10:17:17 -0300

    added context in options for callback calls

--- geo.js
+++ geo.js
@@ -7,6 +7,7 @@ navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess,
         geoprogress = function(){};
     };
     options = options || {};
+    options.context = options.context || this;
 
     var checkLocation = function (position) {
         lastCheckedPosition = position;
@@ -18,7 +19,7 @@ navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess,
             navigator.geolocation.clearWatch(watchID);
             foundPosition(position);
         } else {
-            geoprogress(position);
+            geoprogress.call(options.context, position);
         }
     }
 
@@ -30,11 +31,11 @@ navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess,
     var onError = function (error) {
         clearTimeout(timerID);
         navigator.geolocation.clearWatch(watchID);
-        geolocationError(error);
+        geolocationError.call(options.context, error);
     }
 
     var foundPosition = function (position) {
-        geolocationSuccess(position);
+        geolocationSuccess.call(options.context, position);
     }
 
     if (!options.maxWait)            options.maxWait = 10000; // Default 10 seconds
