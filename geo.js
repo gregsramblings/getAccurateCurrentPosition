6ff55937ca062867ff76254b3ec52f145260d54e
Greg Wilson <gwilson@adobe.com>
2013-08-13 16:44:41 -0400

    Cleaned up code a bit

--- geo.js
+++ geo.js
@@ -1,37 +1,39 @@
 navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess, geolocationError, geoprogress, options) {
-    var lastCheckedPosition;
-    var locationEventCount = 0;
+    var lastCheckedPosition,
+        locationEventCount = 0,
+        watchID,
+        timerID;
     
     options = options || {};
 
     var checkLocation = function (position) {
         lastCheckedPosition = position;
-        ++locationEventCount;
+        locationEventCount = locationEventCount + 1;
         // We ignore the first event unless it's the only one received because some devices seem to send a cached
         // location even when maxaimumAge is set to zero
-        if ((position.coords.accuracy <= options.desiredAccuracy) && (locationEventCount > 0)) {
+        if ((position.coords.accuracy <= options.desiredAccuracy) && (locationEventCount > 1)) {
             clearTimeout(timerID);
             navigator.geolocation.clearWatch(watchID);
             foundPosition(position);
         } else {
             geoprogress(position);
         }
-    }
+    };
 
     var stopTrying = function () {
         navigator.geolocation.clearWatch(watchID);
         foundPosition(lastCheckedPosition);
-    }
+    };
 
     var onError = function (error) {
         clearTimeout(timerID);
         navigator.geolocation.clearWatch(watchID);
         geolocationError(error);
-    }
+    };
 
     var foundPosition = function (position) {
         geolocationSuccess(position);
-    }
+    };
 
     if (!options.maxWait)            options.maxWait = 10000; // Default 10 seconds
     if (!options.desiredAccuracy)    options.desiredAccuracy = 20; // Default 20 meters
@@ -40,6 +42,6 @@ navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess,
     options.maximumAge = 0; // Force current locations only
     options.enableHighAccuracy = true; // Force high accuracy (otherwise, why are you using this function?)
 
-    var watchID = navigator.geolocation.watchPosition(checkLocation, onError, options);
-    var timerID = setTimeout(stopTrying, options.maxWait); // Set a timeout that will abandon the location loop
-}
+    watchID = navigator.geolocation.watchPosition(checkLocation, onError, options);
+    timerID = setTimeout(stopTrying, options.maxWait); // Set a timeout that will abandon the location loop
+};
